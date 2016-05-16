2b1a35b9659d93f643702178ebf45669ae9d91d3
purpletianjing <TianjingSun@outlook.com>
2016-05-16 13:23:40 +0800

    delete tail

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -3,5 +3,4 @@
 set -e
 
 backup_sql_file=BronzeSword_$(date +%Y-%m-%d:%H:%M:%S).sql
-docker exec -it assembly_mysql_1 mysqldump -u$user -p$password BronzeSword > $backup_sql_file
-tail -n +2 $backup_sql_file > $backup_sql_file.tmp && mv $backup_sql_file.tmp $backup_sql_file
+ssh -T $REMOTE_SERVER_IP docker exec -i assembly_mysql_1 "mysqldump -u $user_for_dump -p$password BronzeSword" > $backup_sql_file 
