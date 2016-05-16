60b44fe76ad68f370323ac9bfaa3e5badec64b0b
purpletianjing <TianjingSun@outlook.com>
2016-05-16 13:52:51 +0800

    add backup_path

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-backup_sql_file=BronzeSword_$(date +%Y-%m-%d:%H:%M:%S).sql
-ssh -T $REMOTE_SERVER_IP docker exec -i assembly_mysql_1 "mysqldump -u $user_for_dump -p$password BronzeSword" > $backup_sql_file 
+backup_sql_file=$backup_path/BronzeSword_$(date +%Y-%m-%d:%H:%M:%S).sql
+ssh -T $REMOTE_SERVER_IP docker exec -i assembly_mysql_1 "mysqldump -u $user_for_dump -p$password BronzeSword" > $backup_sql_file
